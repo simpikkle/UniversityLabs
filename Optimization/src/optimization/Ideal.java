2e88c4190050e09aa9bafc8a34c741e55146c3b7
nelltqf <hellnellt@gmail.com>
2017-12-13 11:42:22 +0300

    4th lab

--- Optimization/src/optimization/Ideal.java
+++ Optimization/src/optimization/Ideal.java
@@ -6,41 +6,49 @@ import java.util.List;
 
 public class Ideal implements Lab {
 
-    private static final int COUNT = 3;
+    private static final int countOfCriterias = 3;
     private static List<Item> list = new ArrayList<>();
-    private static Float[] weight = new Float[]{4f, 3f, 3f};
-    private static Boolean[] adv = new Boolean[COUNT];
-    private static Float[] P = new Float[]{1f, 2f, 3f, 4f, 5f};
+    private static Float[] weight = new Float[]{6f, 6f, 2f};
+    private static Boolean[] booleans = new Boolean[countOfCriterias];
+    private static Float[] degreeOfConcentration = new Float[]{1f, 2f, 3f, 4f, 5f};
     private static int count = 0;
-    private static int P_ind = 0;
+    private static int concentrationIndex = 0;
 
     @Override
     public void run() {
-        initializeDefaultList(list);
+        System.out.println("Пример");
+        initializeDefaultList();
+        findBestItem();
 
+        System.out.println("Собственные значения");
+        initializeUniqueItems();
+        findBestItem();
+    }
+
+    private void findBestItem() {
         for (int k = 0; k < list.size() - 1; k++) {
             Item ideal = makeIdeal(list);
-            System.out.print("MKO +\t");
+            System.out.print("MKO (идеальный) \t");
             ideal.printK();
 
             Item worse = makeWorse(list);
-            System.out.print("MKO -\t");
+            System.out.print("MKO (худший) \t");
             worse.printK();
 
             makeD(ideal, worse);
 
-            System.out.println("\n" + "Table D");
+            System.out.println("\n" + "Таблица в относительных единицах измерения: ");
             list.forEach(Item::printD);
 
-            makeL(list);
-            System.out.println("\n" + "Table L");
+            countDistanceToIdeal();
+            System.out.println("\n" + "Таблица с учетом весов и степени концентрации: ");
             for (Item e : list) {
                 e.printL();
             }
 
-            Item[] last = new Item[P.length];
-            for (int i = 0; i < P.length; i++) {
-                P_ind = i;
+            Item[] last = new Item[degreeOfConcentration.length];
+            for (int i = 0; i < degreeOfConcentration.length; i++) {
+                concentrationIndex = i;
                 Collections.sort(list);
                 last[i] = list.get(list.size() - 1);
             }
@@ -55,54 +63,55 @@ public class Ideal implements Lab {
                 }
             }
 
-            System.out.println("После удаления худшего элемента остается");
+            System.out.println("После удаления элементов с наиболее удаленными МКО: ");
             if (allLast) {
                 list.remove(last[0]);
                 k--;
             }
 
             list.stream().map(Item::toString).forEach(System.out::println);
-            System.out.println("_____________________");
+            System.out.println("_________________________________");
         }
 
     }
 
-    private void makeL(List<Item> list) {
+    private void countDistanceToIdeal() {
         for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < P.length; j++) {
-                Float tmp_L = 0f;
-                for (int k = 0; k < COUNT; k++) {
-                    tmp_L += (weight[k] * (1 - list.get(i).arrayD[k]));
+            for (int j = 0; j < degreeOfConcentration.length; j++) {
+                Float tempL = 0f;
+                for (int k = 0; k < countOfCriterias; k++) {
+                    tempL += (weight[k] * (1 - list.get(i).arrayD[k]));
                 }
-                list.get(i).arrayL[j] = (float) Math.pow(tmp_L, 1 / P[j]);
+                list.get(i).arrayL[j] = (float) Math.pow(tempL, 1 / degreeOfConcentration[j]);
             }
-
         }
     }
 
     private void makeD(Item ideal, Item worse) {
         for (int i = 0; i < list.size(); i++) {
-            Float[] tmp_D = new Float[COUNT];
-            for (int j = 0; j < COUNT; j++) {
+            Float[] tmp_D = new Float[countOfCriterias];
+            for (int j = 0; j < countOfCriterias; j++) {
                 if (ideal.arrayK[j] - worse.arrayK[j] != 0)
                     tmp_D[j] = (ideal.arrayK[j] - list.get(i).arrayK[j]) / (ideal.arrayK[j] - worse.arrayK[j]);
                 else tmp_D[j] = 0f;
             }
             list.get(i).setArrayD(tmp_D);
         }
-        Float[] tmp_D = new Float[COUNT];
-        for (int j = 0; j < COUNT; j++) {
+        Float[] tmp_D = new Float[countOfCriterias];
+        for (int j = 0; j < countOfCriterias; j++) {
             tmp_D[j] = (ideal.arrayK[j] - ideal.arrayK[j]) / (ideal.arrayK[j] - worse.arrayK[j]);
-            //System.out.print(tmp_D[j]+" ");
         }
         ideal.setArrayD(tmp_D);
-        for (int j = 0; j < COUNT; j++) {
+        for (int j = 0; j < countOfCriterias; j++) {
             tmp_D[j] = (ideal.arrayK[j] - worse.arrayK[j]) / (ideal.arrayK[j] - worse.arrayK[j]);
         }
         worse.setArrayD(tmp_D);
     }
 
-    private void initializeDefaultList(List<Item> list) {
+    private void initializeDefaultList() {
+        list = new ArrayList<>();
+        count = 0;
+        concentrationIndex = 0;
         list.add(new Item(new Float[]{5.0f, 1.0f, 50.0f}));
         list.add(new Item(new Float[]{10.0f, 1.5f, 40.0f}));
         list.add(new Item(new Float[]{2.0f, 1.5f, 90.0f}));
@@ -110,29 +119,34 @@ public class Ideal implements Lab {
         list.add(new Item(new Float[]{6.0f, 3.0f, 100.0f}));
         list.add(new Item(new Float[]{16.0f, 3.5f, 50.0f}));
 
-        adv = new Boolean[]{false, true, false};
+        booleans = new Boolean[]{true, true, false};
+        weight = new Float[]{6f, 6f, 2f};
     }
 
-    private void initializeUniqueItems(List<Item> list) {
-        list.add(new Item(new Float[]{50.0f, 40.0f, 5.0f}));
-        list.add(new Item(new Float[]{40.0f, 30.0f, 6.0f}));
-        list.add(new Item(new Float[]{75.0f, 60.0f, 5.0f}));
-        list.add(new Item(new Float[]{60.0f, 50.0f, 9.0f}));
-        list.add(new Item(new Float[]{80.0f, 80.0f, 7.0f}));
-        list.add(new Item(new Float[]{70.0f, 60.0f, 4.0f}));
-        list.add(new Item(new Float[]{80.0f, 70.0f, 4.0f}));
-        list.add(new Item(new Float[]{65.0f, 60.0f, 8.0f}));
+    private void initializeUniqueItems() {
+        list = new ArrayList<>();
+        count = 0;
+        concentrationIndex = 0;
+        list.add(new Item(new Float[]{80.0f, 12.0f, 531.0f}));
+        list.add(new Item(new Float[]{10.0f, 16.0f, 120.0f}));
+        list.add(new Item(new Float[]{75.0f, 32.0f, 500.0f}));
+        list.add(new Item(new Float[]{80.0f, 40.0f, 339.0f}));
+        list.add(new Item(new Float[]{34.0f, 12.0f, 123.0f}));
+        list.add(new Item(new Float[]{72.0f, 17.0f, 101.0f}));
+        list.add(new Item(new Float[]{53.0f, 38.0f, 899.0f}));
+        list.add(new Item(new Float[]{39.0f, 87.0f, 1230.0f}));
 
-        adv = new Boolean[]{true, false, true};
+        booleans = new Boolean[]{true, true, false};
+        weight = new Float[]{4f, 2f, 3f};
     }
 
     private Item makeIdeal(List<Item> list) {
-        Float[] tmp = new Float[COUNT];
+        Float[] tmp = new Float[countOfCriterias];
         for (int i = 0; i < tmp.length; i++) {
             Float max = list.get(0).arrayK[i];
             for (int j = 0; j < list.size(); j++) {
-                if (adv[i] && list.get(j).arrayK[i] > max) max = list.get(j).arrayK[i];
-                else if (!adv[i] && list.get(j).arrayK[i] < max) max = list.get(j).arrayK[i];
+                if (booleans[i] && list.get(j).arrayK[i] > max) max = list.get(j).arrayK[i];
+                else if (!booleans[i] && list.get(j).arrayK[i] < max) max = list.get(j).arrayK[i];
             }
             tmp[i] = max;
         }
@@ -140,12 +154,12 @@ public class Ideal implements Lab {
     }
 
     private Item makeWorse(List<Item> list) {
-        Float[] tmp = new Float[COUNT];
+        Float[] tmp = new Float[countOfCriterias];
         for (int i = 0; i < tmp.length; i++) {
             Float min = list.get(0).arrayK[i];
             for (int j = 0; j < list.size(); j++) {
-                if (adv[i] && list.get(j).arrayK[i] < min) min = list.get(j).arrayK[i];
-                else if (!adv[i] && list.get(j).arrayK[i] > min) min = list.get(j).arrayK[i];
+                if (booleans[i] && list.get(j).arrayK[i] < min) min = list.get(j).arrayK[i];
+                else if (!booleans[i] && list.get(j).arrayK[i] > min) min = list.get(j).arrayK[i];
             }
             tmp[i] = min;
         }
@@ -154,21 +168,17 @@ public class Ideal implements Lab {
 
     private static class Item implements Comparable<Item> {
         private int index;
-        private Float[] arrayK = new Float[COUNT];
-        private Float[] arrayD = new Float[COUNT];
-        private Float[] arrayL = new Float[P.length];
+        private Float[] arrayK = new Float[countOfCriterias];
+        private Float[] arrayD = new Float[countOfCriterias];
+        private Float[] arrayL = new Float[degreeOfConcentration.length];
 
         public Item(Float arrayK[]) {
             this.arrayK = arrayK;
             this.index = ++count;
         }
 
-        public void setArrayD(Float[] D) {
-            this.arrayD = D;
-        }
-
         public int compareTo(Item element) {
-            float diff = arrayL[P_ind] - element.arrayL[P_ind];
+            float diff = arrayL[concentrationIndex] - element.arrayL[concentrationIndex];
             if (diff > 0) {
                 return -1;
             } else if (diff < 0) {
@@ -176,6 +186,10 @@ public class Ideal implements Lab {
             } else {
                 return 0;
             }
+        }
+
+        public void setArrayD(Float[] D) {
+            this.arrayD = D;
         }
 
         public String toString() {
