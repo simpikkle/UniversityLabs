9cd62ac480fa0bb55253da088e884c59f4618e92
nelltqf <hellnellt@gmail.com>
2018-02-18 17:46:19 +0300

    Adjustments for calculator

--- javalabs8/src/main/java/DummyCalculator.java
+++ javalabs8/src/main/java/DummyCalculator.java
@@ -2,6 +2,9 @@ public class DummyCalculator implements Calculator {
 
     @Override
     public int sum(int x, int y) {
+        if (x > 0 && y > 0 && x + y < 0 || x < 0 && y < 0 && x + y > 0) {
+            throw new IllegalArgumentException("Augend or addend exceeds the limits");
+        }
         return x + y;
     }
 }
--- javalabs8/src/test/java/CalculatorTest.java
+++ javalabs8/src/test/java/CalculatorTest.java
@@ -11,5 +11,20 @@ public class CalculatorTest {
     public void testSum() {
         assertThat(calculator.sum(1, 2)).isEqualTo(3);
         assertThat(calculator.sum(-1, -2)).isEqualTo(-3);
+        assertThat(calculator.sum(0, -2)).isEqualTo(-2);
+    }
+
+    @Test
+    public void testSumMax() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            calculator.sum(Integer.MAX_VALUE, 1);
+        }).withMessageContaining("Augend or addend exceeds the limits");
+    }
+
+    @Test
+    public void testSumMin() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            calculator.sum(Integer.MIN_VALUE, -1);
+        }).withMessageContaining("Augend or addend exceeds the limits");
     }
 }
